60d7c273baf9c71f096ed9fbe5562ec60db71255
d1temnd <74726976+d1temnd@users.noreply.github.com>
2024-08-06 22:36:39 +0300

    start wright del db tasks

--- backend/api/admin_start_game.py
+++ backend/api/admin_start_game.py
@@ -6,7 +6,7 @@ import json
 admin_start_game_bp = Blueprint('admin_start_game', __name__)
 
 
-@admin_start_game_bp.route("/api/admin/start_game", methods=['POST'])
+@admin_start_game_bp.route("/api/admin/start_game", methods=['POST', 'GET'])
 def admin_start_game():
     # Найти пользователя с данным user_id
     users = Users.query.all()
@@ -29,28 +29,35 @@ def admin_start_game():
         with open('words/task.json', encoding="utf-8") as f:
             data = json.load(f)
 
-        obj = random.choice(data["object"])
-        selected_prop = random.choice(list(obj["prop"].keys()))
-
         action = random.choice(data["action"])
+        obj = random.choice(data["object"])
         place = random.choice(data["place"])
-        prop = random.choice(obj["prop"][selected_prop])
-        obj = random.choice(obj["obj"])
+        topic = random.choice(data["topic"])
 
-        text_task = f'{user1.username}, {user2.username}:\nДействие: {action}.\nОбъект: {prop} {obj}.\nМесто: {place}.'
+        templates = [
+            f"{action} {obj} в {place}",
+            f"В {place} {action} {topic}",
+            f"{action} {obj}, обсуждая {topic}, в {place}"
+        ]
+
+        task = random.choice(templates)
+        text_task = f'{user1.username}, {user2.username}:\n{task}'
         # Выводим пары
 
-        usertask = Tasks(text=text_task)
-        db.session.add(usertask)
+        # db.session.query(Tasks).delete()
+        # db.session.commit()
+
+        user_task = Tasks(text=text_task)
+        db.session.add(user_task)
         db.session.commit()
-        user1.task_id = usertask.id
-        user2.task_id = usertask.id
+        user1.task_id = user_task.id
+        user2.task_id = user_task.id
 
         db.session.add(user1)
         db.session.add(user2)
 
         db.session.commit()
 
-        print(usertask.id)
+        print(user_task.id)
 
     return jsonify({}), 200
