38049dbd1c7ef556fdcab8b878d99f2a310d4fed
d1temnd <74726976+d1temnd@users.noreply.github.com>
2024-08-12 16:49:26 +0700

    last commit

--- backend/main.py
+++ backend/main.py
@@ -2,15 +2,13 @@ from flask import Flask
 from api.models import db
 from api import register_routes
 
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db.init_app(app)
 
-
 register_routes(app)
-
 
 if __name__ == '__main__':
     with app.app_context():
